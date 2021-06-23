f709027569d45c7dad0c76d1125e40214578dc25
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-06-23 12:21:18 -0500

    feat: completed setState call inside the class so that Latitude would appear quicker.

--- src/index.js
+++ src/index.js
@@ -5,16 +5,23 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
+        // THIS IS THE ONLY TIME we do direct assignment to this.state
         this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // in order to update our state object, we called setState
+                this.setState({ lat: position.coords.latitude });
+
+                // don't do this!!!
+                // this.state.lat = position.coords.latitude
+            } ,
+            err => console.log(err)
+        );
     }
 
     // React says that we have to define render!!1
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position) ,
-            err => console.log(err)
-        );
-
         return <div>Latitude: {this.state.lat}</div>;
     }
 }
